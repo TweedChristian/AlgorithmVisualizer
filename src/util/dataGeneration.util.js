9b06cf433f335a53a8e4f5a8f7ba54709db9ad52
Christian Tweed <christiantweed@gmail.com>
2020-11-13 17:09:40 -0500

    Rewrote the random graph algorithm

--- src/util/dataGeneration.util.js
+++ src/util/dataGeneration.util.js
@@ -44,24 +44,39 @@ const completeGraph = (number) => {
     };
 }
 
+const maxSubsetOfEdges = (n) => {
+    return Math.floor(n * (n - 1) / 2);
+}
+
 const randomGraph = (max) => {
     let nodes = {};
     let links = [];
+    const edgeNumber = Math.ceil(Math.random() * maxSubsetOfEdges(max));
     for(let i = 1; i <= max; i++){
         nodes[i] = {
             id: i,
             neighbors: []
         };
-        for(let j = i + 1; j <= max; j++){
-            if(Math.floor((Math.random() * 2))){
-                links.push({
-                    source: i,
-                    target: j
-                });
-                nodes[i].neighbors.push(j);
-            }
-        }
+        // for(let j = i + 1; j <= max; j++){
+        //     if(Math.floor((Math.random() * 2))){
+        //         links.push({
+        //             source: i,
+        //             target: j
+        //         });
+        //         nodes[i].neighbors.push(j);
+        //     }
+        // }
     }
+    for(let i = 0; i < edgeNumber; i++){
+        let source = Math.ceil(Math.random() * max);
+        let target = Math.ceil(Math.random() * max);
+        links.push({
+            source: source,
+            target: target
+        });
+        nodes[source].neighbors.push(target);
+    }
+    
     return {
         nodes,
         links
